d459501fc9c05b71b9e28e5e179c39365adcb73d
Kabuto <591775790@qq.com>
2019-09-18 22:27:34 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <span class="iconfont iconfanhui"></span>
+    <span class="iconfont iconcaidan"></span>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html')
+  let fontsize = window.innerWidth / 10
+  fontsize = (fontsize > 50) ? 50 : fontsize
+  html.style.fontSize = fontsize + 'px'
+})
+</script>
+
+<style>
+
+</style>
